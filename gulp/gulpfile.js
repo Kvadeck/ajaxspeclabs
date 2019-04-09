ff2f4a9ce11d58ea26488d6f2118b3e209a37287
Kvadeck <kvadeck@yadnex.ru>
2019-04-09 10:51:35 +0300

    Gulp config finish#1

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -2,13 +2,13 @@
 
 // Requires
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+// var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
-var useref = require('gulp-useref');
-var gulpIf = require('gulp-if');
-var cssnano = require('gulp-cssnano');
-var imagemin = require('gulp-imagemin');
-var del = require('del');
+// var useref = require('gulp-useref');
+// var gulpIf = require('gulp-if');
+// var cssnano = require('gulp-cssnano');
+// var imagemin = require('gulp-imagemin');
+// var del = require('del');
 
 const reload = browserSync.reload;
 
@@ -16,50 +16,48 @@ const reload = browserSync.reload;
 gulp.task('browserSync', function() {
     browserSync.init({
         proxy: {
-            target: "http://spo.site/2.0",
+            target: "http://localhost:8080",
         },
     })
 });
 
-gulp.task('sass', function() {
-    return gulp.src('../css/2.0/scss/**/*.scss') // Gets all files ending with .scss in ../css/2.0/scss
-        .pipe(sass())
-        .pipe(gulp.dest('../css/2.0/'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
-});
+gulp.task( 'default', [ 'browserSync' ] )
+// gulp.task('sass', function() {
+//     return gulp.src('../css/2.0/scss/**/*.scss') // Gets all files ending with .scss in ../css/2.0/scss
+//         .pipe(sass())
+//         .pipe(gulp.dest('../css/2.0/'))
+//         .pipe(browserSync.reload({
+//             stream: true
+//         }))
+// });
 
 // gulp useref : concatenates any number of CSS and JavaScript files into a single
 // file by looking for a comment that starts with "<!--build:" and ends with //"<!--endbuild-->
 // After that he doing minification in that file.
-gulp.task('useref', function(){
-    return gulp.src('app/*.html')
-        .pipe(useref())
-        .pipe(gulpIf('*.css', cssnano()))
-        .pipe(gulp.dest('dist'))
-});
 
-gulp.task('images', function(){
-    return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
-        .pipe(imagemin({
-            // Setting interlaced to true
-            interlaced: true
-        }))
-        .pipe(gulp.dest('dist/images'))
-});
+// gulp.task('useref', function(){
+//     return gulp.src('app/*.html')
+//         .pipe(useref())
+//         .pipe(gulpIf('*.css', cssnano()))
+//         .pipe(gulp.dest('dist'))
+// });
 
-gulp.task('clean:dist', function() {
-    return del.sync('dist');
-});
+// gulp.task('images', function(){
+//     return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+//         .pipe(imagemin({
+//             // Setting interlaced to true
+//             interlaced: true
+//         }))
+//         .pipe(gulp.dest('dist/images'))
+// });
+
+// gulp.task('clean:dist', function() {
+//     return del.sync('dist');
+// });
 
 // Task Watch
-gulp.task('watch', ['browserSync', 'sass'], function (){
-    gulp.watch('../css/2.0/scss/**/*.scss', ['sass']);
-    gulp.watch([
-        '../../app/Resources/**/*.html.twig'
-    ]).on('change', reload);
+    // gulp.watch('../css/2.0/scss/**/*.scss', ['sass']);
+    // gulp.watch(['../']).on('change', reload);
     // Other watchers
-});
-
-
+    
+    gulp.watch("../**/*.html").on("change", reload);
